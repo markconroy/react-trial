e704a71db69eca83482c4a7d8528f6b0a25b8f54
Mark Conroy <info@adesignforlife.net>
2018-12-05 14:52:58 +0000

    flexbox the layout for sticky footer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import React, {
 import './App.css';
 import Header from './components/global/header/Header';
 import Router from "./components/global/router/Router";
+import Footer from "./components/global/footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Router />
+        <main>
+          <Router />
+        </main>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/global/footer/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <footer className="footer">
+          <p>We can put some footer stuff in here.</p>
+        </footer>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Footer;
