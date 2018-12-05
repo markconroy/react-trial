5207b8cd9ae6cbe546ce75504e8f0eb7f73aa427
Mark Conroy <info@adesignforlife.net>
2018-12-05 14:53:22 +0000

    Updates

---
+++ precache-manifest.19c3edbddbfeb1028ddbf8a961987f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac8ebeb719e45f449a8114c8bf54641",
+    "url": "./static/media/logo.fac8ebeb.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5341b1eb44702d9fe941",
+    "url": "./static/js/main.5341b1eb.chunk.js"
+  },
+  {
+    "revision": "41284efeee12cf751bc1",
+    "url": "./static/js/1.41284efe.chunk.js"
+  },
+  {
+    "revision": "5341b1eb44702d9fe941",
+    "url": "./static/css/main.0c4f7223.chunk.css"
+  },
+  {
+    "revision": "6da38b92775b9001b8e872062d4df661",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-trial/build/precache-manifest.c89cf16137cf198bab8e2257e8e03ae9.js"
+  "./precache-manifest.19c3edbddbfeb1028ddbf8a961987f97.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-trial/build/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
